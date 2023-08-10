3bdd2b1e8943cf447c6a5516bef391cbfe3e5c48
charliepoker <iheanachocharlie@gmail.com>
2023-08-10 06:15:08 -0400

    added if statement

--- src/main/java/chapter3/SalaryCalculator.java
+++ src/main/java/chapter3/SalaryCalculator.java
@@ -1,2 +1,32 @@
-package chapter3;public class SalaryCalculator {
+package chapter3;
+
+import java.util.Scanner;
+
+/*
+IF Statement.
+All salespeople get a payment of $1000 a week.
+Salespeople who exceed 10 sales get an additional bonus of $250
+ */
+public class SalaryCalculator {
+
+
+    public static void main (String args[]){
+
+//        Initialize the known values
+        int salary = 1000;
+        int bonus = 250;
+        int quota = 10;
+
+//        Get values for the unknown
+        System.out.println("How much sales did the employee make this year");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+
+//        Quick detour for the bonus earners
+        if(sales > quota){
+           salary = salary + bonus;
+        }
+//        Output
+        System.out.println("The employee's pay is $" + salary);
+    }
 }
