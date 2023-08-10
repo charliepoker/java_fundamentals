c33975362fc65802b44c6081bdc4a66160beeff7
charliepoker <iheanachocharlie@gmail.com>
2023-08-10 05:45:32 -0400

    first commit

---
+++ src/main/java/chapter2/Madlibs.java
@@ -0,0 +1,27 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class Madlibs {
+
+    public static void main (String arg[]){
+
+//        1. Ask the user for the season of the yr
+        System.out.println("What is the season of the year?");
+        Scanner scanner = new Scanner(System.in);
+        String season = scanner.next();
+
+//        2. Assign a variable
+        System.out.println("Enter a number");
+        int number = scanner.nextInt();
+
+//        3. Assign an adjective to a variable
+        System.out.println("Enter an Adjective");
+        String adjective = scanner.next();
+
+//        4. Output the result
+
+        System.out.println("On a " + adjective + " " + season + " day, I drink a minimum of " + number + " coffee."   );
+
+    }
+}
