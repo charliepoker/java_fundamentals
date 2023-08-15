21cc4212adc3a6567023a8716125d17c3b6e811a
charliepoker <iheanachocharlie@gmail.com>
2023-08-15 10:59:06 -0400

    sorted the array

--- src/main/java/chapter7/LotteryTicket.java
+++ src/main/java/chapter7/LotteryTicket.java
@@ -1,5 +1,6 @@
 package chapter7;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class LotteryTicket {
@@ -7,6 +8,7 @@ public class LotteryTicket {
     private static final int MAX_TICKET_NUMBER = 69;
     public static void main(String[] args){
         int[] ticket = generateNumbers();
+        Arrays.sort(ticket);
         printTicket(ticket);
     }
 
@@ -15,10 +17,35 @@ public class LotteryTicket {
         Random random = new Random();
 
         for(int i = 0; i < LENGTH; i++){
-            ticket[i] = random.nextInt(MAX_TICKET_NUMBER) + 1;
+            int randomNumber;
+//            Generate random number and then make sure it doesn't already exist in the array. if it does, regenerate search again
+            do {
+                randomNumber = random.nextInt(MAX_TICKET_NUMBER) + 1;
+            }while(search(ticket,randomNumber));
+
+//            The Number is unique if we get here. Add it to the array.
+            ticket[i] = randomNumber;
         }
 
         return ticket;
+    }
+
+    /**
+     * Does a sequential search on an array to find a value
+     * @param array: Array to search through
+     * @param numberToSearchFor: Value to search for
+     * @return: true if found, false if not
+     */
+    public static boolean search (int[] array,int numberToSearchFor){
+//        This is an enhanced loop. it iterates through array and each time assigns the current element to 'value'
+        for(int value : array){
+            if(value  == numberToSearchFor){
+                return true;
+            }
+        }
+//        If we've reached this point, then the entire array was searched and the value was not found
+        return false;
+
     }
 
     public  static void printTicket (int[] ticket){
